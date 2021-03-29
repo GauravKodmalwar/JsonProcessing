67cbc34d5e1db24d65da9b3c14d70064799d9907
gaurav kodmalwar <gkodmalwar@qualys.com>
2021-03-29 08:52:46 +0530

    Initial commit

---
+++ formatting.py
@@ -0,0 +1,107 @@
+import json
+from re import compile, sub
+
+input = """
+{
+    "win_iis_agent": {
+        "Default Web Site": {
+            "_id": "Memory Usage",
+            "Memory Usage": 2713.0
+        },
+        "dotnettest": {
+            "_id": "Memory Usage",
+            "Memory Usage": 2713.0
+        },
+        "Default Web Site": {
+            "_id": "Free Memory percent",
+            "Free Memory percent": 41.0
+        },
+        "dotnettest": {
+            "_id": "Free Memory percent",
+            "Free Memory percent": 41.6
+        },
+        "Default Web Site": {
+            "_id": "Used Memory percent",
+            "Used Memory percent": 67.0
+        },
+        "dotnettest": {
+            "_id": "Used Memory percent",
+            "Used Memory percent": 67.0
+        },
+        "Default Web Site": {
+            "_id": "Total (Mb)",
+            "Total (Mb)": 4095.0
+        },
+        "dotnettest": {
+            "_id": "Total (Mb)",
+            "Total (Mb)": 4095.0
+        }
+    },
+    "Linux_java_agent": {}
+}
+"""
+# Patterns To Deal With Keys
+pattern1 = compile(r'[{}]')
+pattern2 = compile(r'[":,\s]')
+pattern3 = compile(r'[":,]')
+
+# For Handeling Duplicate Keys and adding to a list with that key
+class ModifiedDict(dict):
+    def __setitem__(self, key, value):
+        try:
+            self[key].append(value)
+        except KeyError:
+            super(ModifiedDict, self).__setitem__(key, value)
+        except AttributeError:
+            super(ModifiedDict, self).__setitem__(key, [self[key], value])
+
+# To Update The Json Data
+def Update_Json(data_Dict):
+    formatted_data = json.loads('')
+    for dataDict in data_Dict:
+        uniQKeys = dataDict.keys()
+        for key in formatted_data.keys():
+            if formatted_data[key].keys():
+                for key1 in formatted_data[key].keys():
+                    if key1.replace(' ', '') in uniQKeys:
+                        formatted_data[key][key1] = dataDict[key1.replace(
+                            ' ', '')]
+    return formatted_data
+
+# To Extract Duplicate keys value pairs
+def Extract_Update(mInput, output_file_name=None):
+    lines = []
+    top_level_keys = []
+    lines = [sub(pattern1, '', line.replace('\n', '').replace('\t', ''))
+                 for line in mInput.split('\n')]
+
+    top_level_keys = list(json.loads(mInput).keys())
+
+    lines[-2] = '"optional_key": '
+    top_level_keys = top_level_keys[1:]
+    top_level_keys.append("optional_key")
+
+    dataDict = ModifiedDict()
+    keys = []
+    values = []
+    dict_list = []
+    for line in lines:
+        if line != '' and line != ',':
+            if line[0] == '"' and line[-2] == ':':
+                key = sub(pattern2, '', line)
+                keys.append(key)
+
+                if key in top_level_keys:
+                    dict_list.append(dataDict)
+                    dataDict = ModifiedDict()
+            else:
+                values.extend(line.split(':'))
+                values[-2] = sub(pattern3, '', values[-2])
+                values[-1] = sub(pattern3, '', values[-1])
+                if len(values) == 4:
+                    dataDict[keys[-1]] = dict({values[0]: values[1], values[2]: float(values[3])})
+                    values = []
+    return Update_Json(dict_list)
+
+if __name__ == '__main__':
+    print(Extract_Update(input))
